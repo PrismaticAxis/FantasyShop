b14217b5891f30a328f6820006e4893cf9b5a104
PrismaticAxis <PrismaticAxis@gmail.com>
2020-09-01 11:41:11 -0600

    Upload Player class

---
+++ Player.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ * @author	Stephen Lovell
+ * @since	01 Sep 2020
+ *
+ */
+public class Player
+{
+	private int funds;
+	private Merchandise[] inventory;
+	
+	
+	
+	public Player()
+	{
+		
+	}
+	
+	//needs initialization values
+	public Player(int funds, int inventorySize)
+	{
+		
+	}
+	
+	
+	
+	public int getFunds()
+	{
+		return funds;
+	}
+	
+	public void setFunds(int amount)
+	{
+		funds = amount;
+	}
+}
