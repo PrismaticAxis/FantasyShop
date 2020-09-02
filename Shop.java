3a40f3b913caee4d89d11038decef774654ba7ec
PrismaticAxis <PrismaticAxis@gmail.com>
2020-09-02 12:00:30 -0600

    Set up Armor menu

--- Shop.java
+++ Shop.java
@@ -14,7 +14,6 @@ public class Shop
 	
 	public static void main(String[] args) throws FileNotFoundException
 	{
-		Armor[] armor = stockArmor();
 		mainMenu();
 	}
 	
@@ -24,25 +23,8 @@ public class Shop
 		{
 			Scanner input = new Scanner(System.in);
 			
-			System.out.println("x)\t[Exit]");
-			System.out.println("h)\t[Help]");
-			System.out.println("i)\t[View Inventory]");
-			System.out.println();
-			
-			for (int i = 0; i < CATEGORIES.length; i++)
-			{
-				System.out.print(i + 1);
-				System.out.print(")\t");
-				System.out.print(CATEGORIES[i]);
-				System.out.println();
-			}
-			
-			System.out.println();
-			System.out.println(
-					"[Funds: " + SILVER + player.getFunds() + ']');
-			
-			System.out.println();
-			System.out.println(">");
+			System.out.println("Main Menu");
+			printMenu(CATEGORIES);
 			
 			String choice = input.next();
 			switch (choice)
@@ -54,6 +36,7 @@ public class Shop
 			case "i":
 				break;
 			case "1":	//Armor
+				armorMenu();
 				break;
 			case "2":	//Weapons
 				break;
@@ -72,6 +55,85 @@ public class Shop
 			System.out.println(e.getMessage());
 			mainMenu();
 		}
+	}
+	
+	public static void armorMenu() throws FileNotFoundException
+	{
+		try
+		{
+			Armor[] armor = stockArmor();
+			String[] armorNames = new String[armor.length];
+			for (int i = 0; i < armorNames.length; i++)
+				armorNames[i] = armor[i].getName();
+			
+			Scanner input = new Scanner(System.in);
+			
+			System.out.println("Armor");
+			System.out.println("b)\t[Back]");
+			printMenu(armorNames);
+			
+			String choice = input.next();
+			
+			switch (choice)
+			{
+			case "b":
+				mainMenu();
+				break;
+			case "x":
+				break;
+			case "h":
+				break;
+			case "i":
+				break;
+			case "1":
+				break;
+			case "2":
+				break;
+			case "3":
+				break;
+			case "4":
+				break;
+			case "5":
+				break;
+			case "6":
+				break;
+			default:
+				throw new Exception("Invalid input");
+			}
+			
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+			armorMenu();
+		}
+	}
+	
+	public static void printMenu(String[] options)
+	{
+		System.out.println("x)\t[Exit]");
+		System.out.println("h)\t[Help]");
+		System.out.println("i)\t[View Inventory]");
+		System.out.println();
+		
+		for (int i = 0; i < options.length; i++)
+		{
+			System.out.print(i + 1);
+			System.out.print(")\t");
+			System.out.print(options[i]);
+			System.out.println();
+		}
+		
+		System.out.println();
+		System.out.println(
+				"[Funds: " + SILVER + player.getFunds() + ']');
+		
+		System.out.println();
+		System.out.println(">");
 	}
 	
 	//to do: fill in long descriptions
