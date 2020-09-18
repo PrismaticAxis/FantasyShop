5c63701e2081cfa452bd411fd9498c0b4fc96c26
PrismaticAxis <PrismaticAxis@gmail.com>
2020-09-18 13:45:43 -0600

    Fix sell() method

--- Player.java
+++ Player.java
@@ -1,7 +1,10 @@
 /**
  * 
  * @author	Stephen Lovell
- * @since	01 Sep 2020
+ * @since	
+ * @param funds the amount of money the player has
+ * @param inventory the items the player has bought
+ * @param itemsInInventory the number of items the player has bought
  *
  */
 public class Player
@@ -12,11 +15,22 @@ public class Player
 	
 	
 	
+	/**
+	 * Creates a new Player with 5000 starting funds
+	 * and an inventory size of 30
+	 */
 	public Player()
 	{
 		this(5000, 30);
 	}
 	
+	/**
+	 * Creates a new Player with the specified amount of starting funds
+	 * and inventory size
+	 * @param funds the starting amount of money
+	 * @param inventorySize the number of items the player can have
+	 * in their inventory
+	 */
 	public Player(int funds, int inventorySize)
 	{
 		setFunds(funds);
@@ -25,21 +39,40 @@ public class Player
 	
 	
 	
+	/**
+	 * Returns the amount of money the player has
+	 * @return the amount of money the player has
+	 */
 	public int getFunds()
 	{
 		return funds;
 	}
 	
+	/**
+	 * Sets the amount of money the player has
+	 * @param amount the amount of money to set
+	 */
 	public void setFunds(int amount)
 	{
 		funds = amount;
 	}
 	
+	/**
+	 * Checks whether the player can afford a particular item
+	 * @param item the item the player is attempting to buy
+	 * @return true if the players funds is greater than the item's cost
+	 */
 	public boolean hasEnoughFunds(Merchandise item)
 	{
 		return funds >= item.getCost();
 	}
 	
+	/**
+	 * Adds a specified item to the player's inventory, adjusts the
+	 * count of items in the player's inventory, and subtract's the
+	 * item's cost from the player's funds
+	 * @param item the item being bought
+	 */
 	public void buy(Merchandise item)
 	{
 			funds = funds - item.getCost();
@@ -47,8 +80,15 @@ public class Player
 			itemsInInventory++;
 	}
 	
+	/**
+	 * Sells the item at the specified index of the player's inventory
+	 * and adds the item's cost to the player's funds
+	 * @param index the index in the player's inventory that contains
+	 * the item to be sold
+	 */
 	public void sell(int index)
 	{
+		setFunds(getFunds() + inventory[index].getCost());
 		inventory[index] = null;
 		itemsInInventory--;
 		while (index < itemsInInventory)
